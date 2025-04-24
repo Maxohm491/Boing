8cf1160c146abf66ac65a82928343eae368af160
Maxohm491 <maxdemohm@gmail.com>
2025-04-24 12:37:03 -0400

    Folder structure

---
+++ Engine/src/engine.d
@@ -0,0 +1,9 @@
+module Engine.src.engine;
+
+import std.stdio;
+
+// This will be to actually play the game
+void main()
+{
+    writeln("Hello world");
+}
---
+++ Engine/src/leveleditor.d
@@ -0,0 +1,3 @@
+module Engine.src.leveleditor;
+
+// This will be the level editor/tilemap editor
